e3beb9346f1bb10ba2b1ea9ac62c63502ce5eced
gustavoGarciaPereira <gusgurtavo@gmail.com>
2021-03-11 11:21:08 -0300

    execicio um struct

--- struct/execicio1.c
+++ struct/execicio1.c
@@ -31,23 +31,35 @@ struct hm {
 */
 
 
-data fimEvento(data inicio, int diracao_dias){
+data fimEvento(data inicio, int duracao_dias){
     data fim;
     int dias = 0;
     int mes = 0;
-    if((diracao_dias/30)!=0){
-        mes = diracao_dias/30;    
-    }else{
-        
+
+    if(duracao_dias / 365 != 0){
+        inicio.ano += duracao_dias /365;
+        //inicio.dia += 0;
+    }
+    if(duracao_dias / 30 != 0){
+        inicio.mes += duracao_dias / 30;
+        //printf("%d",duracao_dias);
+        if(inicio.mes > 12){
+            inicio.ano += 1;
+            inicio.mes = 1;
+        }
+        duracao_dias -= (duracao_dias / 30)*30;
+        inicio.dia += duracao_dias;
+    }
+
+    if(duracao_dias < 30){
+        inicio.dia += duracao_dias;
     }
     
-    int f;
-    printf("(%d)",dias);
-
     
-    //fim.dia = //2;
-    //fim.mes = //12;
-    //fim.ano = //2020;
+
+    fim.dia = inicio.dia;
+    fim.mes = inicio.mes;
+    fim.ano = inicio.ano;
 
     return fim;
 }
@@ -55,22 +67,23 @@ data fimEvento(data inicio, int diracao_dias){
 ///funcao auciliar
 
 void mostrar_data(data d){
-    printf("%d/%d/%d\n",d.dia,d.mes,d.ano);
+    printf("\n\n\n\n: %d/%d/%d\n",d.dia,d.mes,d.ano);
 }
 
 int main(int argc, char const *argv[])
 {
 
     data x, data_c;
-    int diracao_dias = 40;
+    int duracao_dias = 20;
 
     
-    x.dia = 20;
-    x.mes = 23;
+    x.dia = 1;
+    x.mes = 1;
     x.ano = 2020;
 
 
-    data_c = fimEvento(x, diracao_dias);
+    data_c = fimEvento(x, duracao_dias);
+    mostrar_data(data_c);
     //mostrar_data(data_c);
 
     return 0;
