527ec184693b9aa1cbe2283de5c6b589e86fe5df
gustavoGarciaPereira <gusgurtavo@gmail.com>
2021-03-14 13:45:36 -0300

    fazendo exercicio lista ligada

--- lista_ligada/execicio1.c
+++ lista_ligada/execicio1.c
@@ -31,20 +31,20 @@ typedef struct reg{
 
 int contando_no_recursivo(celula *le, int soma){
     if(le == NULL){
-        return 1;
+        return soma;
     }else{
-        printf(" le->%d | soma = %d\n",le->conteudo,soma);
-        return soma + contando_no_recursivo(le->prox,soma++);
+        printf(" le->%d | soma = %d\n",le->conteudo, soma);
+        return soma + contando_no_recursivo(le->prox, soma++);
     }
 }
+
 int contando_no_for(celula *le){
     int cont = 0;
     for(celula *p = le; p != NULL;p = p->prox){
         cont+=1;
         printf("no(%d)\n",p->conteudo);
     }
-    return cont;
-        
+    return cont; 
 }
 
 
